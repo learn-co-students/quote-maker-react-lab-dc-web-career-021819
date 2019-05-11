de41e497bf40560883325a15a870ba977130187b
Mickey <micksheridan.24@gmail.com>
2019-05-11 12:19:21 -0400

    submit

--- src/actions/quotes.js
+++ src/actions/quotes.js
@@ -1,1 +1,18 @@
 // TODO: Create action creators as defined in tests
+
+function addQuote(quote) {
+  return { type: "ADD_QUOTE", quote: quote };
+}
+function removeQuote(quote) {
+  return { type: "REMOVE_QUOTE", quoteId: quote };
+}
+
+function upvoteQuote(quote) {
+  return { type: "UPVOTE_QUOTE", quoteId: quote };
+}
+
+function downvoteQuote(quote) {
+  return { type: "DOWNVOTE_QUOTE", quoteId: quote };
+}
+
+export { addQuote, removeQuote, upvoteQuote, downvoteQuote };
